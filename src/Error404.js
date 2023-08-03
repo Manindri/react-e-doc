7e23a6310f3f682824e9dc7d58c5c88349cedbae
Manindri <IT20246778@my.sliit.lk>
2023-08-03 07:20:35 -0700

    add home page

--- src/Error404.js
+++ src/Error404.js
@@ -7,14 +7,14 @@ const Error404 = () => {
       <div id="notfound">
         <div className="notfound">
           <div className="notfound-404">
-            <h1>404</h1>
+            <h1></h1>
           </div>
-          <h2>WE ARE SORRY, PAGE NOT FOUND!</h2>
+          <h2>E DOC - "We care for your health"</h2>
           <p>
-            THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN REMOVED HAD ITS NAME
-            CHANGED OR IS TEMPORARILY UNAVAILABLE.
+            Make doctor appointments through us.
+
           </p>
-          <NavLink to="/">back to homepage</NavLink>
+          <NavLink to="/">Make An Appointment</NavLink>
         </div>
       </div>
     </>
